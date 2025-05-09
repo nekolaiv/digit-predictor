0885fe1057d488be628f81118e6781cc1465cc2a
nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2025-05-10 07:44:14 +0800

    update: change urls as needed

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 from . import views
+from .views import RedirectAuthenticatedUserLoginView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', views.register_view, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('register/done/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/registration_complete.html'), name='registration_complete'),
+    path('login/', RedirectAuthenticatedUserLoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 
     # Password reset
-    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='accounts/password_reset.html'), name='password_reset'),
+    path('password_reset/', views.password_reset, name='password_reset'),
     path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'), name='password_reset_complete'),
