0f6829e3d26fbf509c906d29abb317e6296e5219
nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2025-05-09 09:11:40 +0800

    feature: add accounts app for auth

---
+++ accounts/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+
+# Create your views here.
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # or wherever
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/register.html', {'form': form})
