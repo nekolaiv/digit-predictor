0b917a9e8bf66c3e350f799d47684a28ad1bbdab
nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2025-05-10 08:07:07 +0800

    update: add urls for main features

--- predict/urls.py
+++ predict/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('home/', views.home, name='home'),
+    path('predict/ajax/', views.predict_digit, name='predict_digit'),
+    path('dashboard/', views.dashboard_view, name='dashboard'),
 ]
