ee31630705ae424cf94f9748c5d851c976336744
nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2025-05-10 08:34:50 +0800

    update: add error messages

--- accounts/views.py
+++ accounts/views.py
@@ -32,15 +32,19 @@ def password_reset(request):
     if request.method == 'POST':
         username = request.POST['username']
         new_password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
 
         try:
             user = User.objects.get(username=username)
 
             if user.check_password(new_password):
                 messages.error(request, "New password cannot be the same as the old password.")
+            elif new_password != confirm_password:
+                messages.error(request, "The new password and confirmation password do not match.")
             else:
                 user.set_password(new_password)
                 user.save()
+                messages.success(request, "Password reset successful. You can now log in.")
                 return redirect('password_reset_complete')
         except User.DoesNotExist:
             messages.error(request, "No account found with that username.")
@@ -48,4 +52,11 @@ def password_reset(request):
     return render(request, 'accounts/password_reset.html')
 
 
+class CustomLoginView(LoginView):
+    template_name = 'accounts/login.html'
 
+    def form_invalid(self, form):
+        if form.errors.get('password', None):
+            form.add_error(None, "Incorrect password. Please try again.")
+            return self.render_to_response(self.get_context_data(form=form, error_image="path/to/your/image.jpg"))
+        return super().form_invalid(form)
